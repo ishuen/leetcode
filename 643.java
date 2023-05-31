5424b3dc218a8f9ed530bfafa9622858fa4c4e90
ishuen <12228644+ishuen@users.noreply.github.com>
2023-05-31 14:23:13 +0800

    643 909

---
+++ 643.java
@@ -0,0 +1,44 @@
+// 643. Maximum Average Subarray I
+//
+// You are given an integer array nums consisting of n elements, and an integer k.
+//
+// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+// Example 2:
+//
+// Input: nums = [5], k = 1
+// Output: 5.00000
+//
+//
+// Constraints:
+//
+// n == nums.length
+// 1 <= k <= n <= 105
+// -104 <= nums[i] <= 104
+//
+// Runtime 3 ms Beats 93.31%
+// Memory 54.6 MB Beats 57.93%
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int index = 0;
+        int sum = 0;
+        for (int i = 0; i < k - 1; i++) {
+            sum = sum + nums[i];
+        }
+        int max = Integer.MIN_VALUE;
+        while (index <= nums.length - k) {
+            sum = sum + nums[index + k - 1];
+            max = Math.max(max, sum);
+            sum = sum - nums[index];
+            index++;
+        }
+        return (double)max / k;
+    }
+}
